145a7cbb759baf63b44b0f8e0c9e9600ea287083
Thepaeth <theera.cpe12@gmail.com>
2023-01-14 14:21:14 +0700

    add router and mlog for gets method

--- pocket/get.go
+++ pocket/get.go
@@ -4,17 +4,23 @@ import (
 	"database/sql"
 	"net/http"
 
+	"github.com/kkgo-software-engineering/workshop/mlog"
 	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
 )
 
 func (h *handler) GetAllCloudPocket(c echo.Context) error {
-	stmt, err := h.db.Prepare("SELECT id, name, catagory, currency, balance FROM pockets")
+	logger := mlog.L(c)
+	stmt, err := h.db.Prepare("SELECT id, name, category, currency, balance FROM pockets")
+	defer logger.Sync()
 	if err != nil {
+		logger.Error("Can't prepare query all cloud pockets statment", zap.Error(err))
 		return c.JSON(http.StatusInternalServerError, Err{Message: "Can't prepare query all cloud pockets statment"})
 	}
 
 	rows, err := stmt.Query()
 	if err != nil {
+		logger.Error("Can't query all cloud pockets", zap.Error(err))
 		return c.JSON(http.StatusInternalServerError, Err{Message: "Can't query all cloud pockets"})
 	}
 
@@ -23,6 +29,7 @@ func (h *handler) GetAllCloudPocket(c echo.Context) error {
 		var cpk pocket
 		err = rows.Scan(&cpk.ID, &cpk.Name, &cpk.Category, &cpk.Currency, &cpk.Balance)
 		if err != nil {
+			logger.Error("Can't scan cloud pocket", zap.Error(err))
 			return c.JSON(http.StatusInternalServerError, Err{Message: "Can't scan cloud pocket"})
 		}
 		pockets = append(pockets, cpk)
@@ -31,9 +38,12 @@ func (h *handler) GetAllCloudPocket(c echo.Context) error {
 }
 
 func (h *handler) GetCloudPocketByID(c echo.Context) error {
+	logger := mlog.L(c)
 	id := c.Param("id")
-	stmt, err := h.db.Prepare("SELECT id, name, catagory, currency, balance FROM pockets WHERE id = $1")
+	stmt, err := h.db.Prepare("SELECT id, name, category, currency, balance FROM pockets WHERE id = $1")
+	defer logger.Sync()
 	if err != nil {
+		logger.Error("Can't prepare query cloud pocket statment", zap.Error(err))
 		return c.JSON(http.StatusInternalServerError, Err{Message: "Can't prepare query cloud pocket statment"})
 	}
 	row := stmt.QueryRow(id)
@@ -41,10 +51,12 @@ func (h *handler) GetCloudPocketByID(c echo.Context) error {
 	err = row.Scan(&cpk.ID, &cpk.Name, &cpk.Category, &cpk.Currency, &cpk.Balance)
 	switch err {
 	case sql.ErrNoRows:
+		logger.Error("Cloud Pocket Data Not Found", zap.Error(err))
 		return c.JSON(http.StatusNoContent, Err{Message: "Cloud Pocket Data Not Found"})
 	case nil:
 		return c.JSON(http.StatusOK, cpk)
 	default:
+		logger.Error("Can't scan cloud pocket", zap.Error(err))
 		return c.JSON(http.StatusInternalServerError, Err{Message: "Can't scan cloud pocket"})
 	}
 }

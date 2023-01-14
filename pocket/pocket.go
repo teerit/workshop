fc84f323b6cf300c1ade9d71e95021b80c99d650
sirawatB <sirawat.is@nocnoc.com>
2023-01-14 11:36:41 +0700

    init pocket

---
+++ pocket/pocket.go
@@ -0,0 +1,9 @@
+package pocket
+
+type pocket struct {
+	ID       int64   `json:"id"`
+	Name     string  `json:"name"`
+	Category string  `json:"category"`
+	Currency string  `json:"currency"`
+	Balance  float64 `json:"balance"`
+}

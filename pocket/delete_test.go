b29a7fc88d2d37b8a42e81482503cfb5b1d4860e
SThanutworapat <saruth.thanutworapat@gmail.com>
2023-01-14 15:35:14 +0700

    Add test success

--- pocket/delete_test.go
+++ pocket/delete_test.go
@@ -60,3 +60,29 @@ func TestDeleteCloudPocketBalanceNotEqualsZero(t *testing.T) {
 		assert.Equal(t, http.StatusBadRequest, res.Code)
 	}
 }
+
+func TestDeleteCloudPocketSuccess(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	mockRows := sqlmock.NewRows([]string{"id", "name", "category", "currency", "balance"}).
+		AddRow(1, "Test Pocket", "Saving", "THB", 0)
+
+	mock.ExpectQuery("select id, name, category, currency, balance from pockets where id = \\$1").WithArgs(sqlmock.AnyArg()).WillReturnRows(mockRows)
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodDelete, "/cloud-pocket", nil)
+	res := httptest.NewRecorder()
+
+	c := e.NewContext(req, res)
+	c.SetPath("/:id")
+	c.SetParamNames("id")
+	c.SetParamValues("1")
+	h := &handler{db: db}
+	h.Delete(c)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusAccepted, res.Code)
+	}
+}

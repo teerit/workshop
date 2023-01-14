3ee99460487203aa236b67d9d248596674e42d61
Thepaeth <theera.cpe12@gmail.com>
2023-01-14 13:19:22 +0700

    update GetAllCloudPocket use handler

--- pocket/get.go
+++ pocket/get.go
@@ -1,29 +1,44 @@
 package pocket
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
 
-func GetAllCloudPocket(c echo.Context) error {
-	dumpData := []pocket{
-		{
-			ID:       12345,
-			Name:     "Travel Fund",
-			Category: "Vacation",
-			Currency: "THB",
-			Balance:  100,
-		},
-		{
-			ID:       67890,
-			Name:     "Savings",
-			Category: "Emergency Fund",
-			Currency: "THB",
-			Balance:  200,
-		},
+	"github.com/labstack/echo/v4"
+)
+
+func (h *handler) GetAllCloudPocket(c echo.Context) error {
+	stmt, err := h.db.Prepare("SELECT id, name, catagory, currency, balance FROM pockets")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "Can't prepare query all cloud pockets statment"})
 	}
 
-	return c.JSON(200, dumpData)
+	rows, err := stmt.Query()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "Can't query all cloud pockets"})
+	}
+
+	pockets := []pocket{}
+	for rows.Next() {
+		var pck pocket
+		err = rows.Scan(&pck.ID, &pck.Name, &pck.Category, &pck.Currency, &pck.Balance)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, Err{Message: "Can't scan cloud pocket"})
+		}
+		pockets = append(pockets, pck)
+	}
+	return c.JSON(http.StatusOK, pockets)
 }
 
 func GetCloudPocketByID(c echo.Context) error {
+	// id := c.Param("id")
+	// stmt, err := db.
+	dumpData := pocket{
+		ID:       12345,
+		Name:     "Travel Fund",
+		Category: "Vacation",
+		Currency: "THB",
+		Balance:  100,
+	}
 
-	return nil
+	return c.JSON(http.StatusOK, dumpData)
 }

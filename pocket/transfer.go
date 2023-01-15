6938bd4a862879291953c024d9be325876ddbfe2
sirawatB <sirawat.is@nocnoc.com>
2023-01-15 13:01:22 +0700

    finish refactoring transfer

--- pocket/transfer.go
+++ pocket/transfer.go
@@ -1,7 +1,6 @@
 package pocket
 
 import (
-	"database/sql"
 	"github.com/labstack/echo/v4"
 	"log"
 	"net/http"
@@ -22,13 +21,27 @@ type transferResponse struct {
 }
 
 func (h *handler) Transfer(c echo.Context) error {
+	// init db and variable
 	tDto := &transferDto{}
 	sourcePocket := &Pocket{}
 	destPocket := &Pocket{}
-	tfService := newTransferService(h.db)
+	db, err := h.db.Begin()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, errorResp{
+			Status:       "Failed",
+			ErrorMessage: "Internal server error",
+		})
+	}
+	defer func() {
+		if err != nil {
+			_ = db.Rollback()
+		}
+		_ = db.Commit()
+	}()
+	tfService := newTransferService(db)
 
 	// bind dto
-	err := c.Bind(tDto)
+	err = c.Bind(tDto)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, errorResp{
 			Status:       "Failed",
@@ -37,7 +50,7 @@ func (h *handler) Transfer(c echo.Context) error {
 	}
 
 	// query and bind source pocket
-	err = findPocket(h.db, tDto.SourcePocketId, sourcePocket)
+	err = tfService.findPocket(tDto.SourcePocketId, sourcePocket)
 	if err != nil {
 		log.Println(err)
 		return c.JSON(http.StatusNotFound, errorResp{
@@ -46,7 +59,7 @@ func (h *handler) Transfer(c echo.Context) error {
 		})
 	}
 	// query and bind destination pocket
-	err = findPocket(h.db, tDto.DestPocketId, destPocket)
+	err = tfService.findPocket(tDto.DestPocketId, destPocket)
 	if err != nil {
 		return c.JSON(http.StatusNotFound, errorResp{
 			Status:       "Failed",
@@ -68,24 +81,8 @@ func (h *handler) Transfer(c echo.Context) error {
 		})
 	}
 
-	begin, err := h.db.Begin()
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, errorResp{
-			Status:       "Failed",
-			ErrorMessage: "Internal server error",
-		})
-	}
-	defer func() {
-		switch err {
-		case nil:
-			err = begin.Commit()
-		default:
-			begin.Rollback()
-		}
-	}()
-
 	// update amount source and destination
-	err = updatePocket(begin, sourcePocket, sourcePocket.Balance-tDto.Amount)
+	err = tfService.updatePocket(sourcePocket, sourcePocket.Balance-tDto.Amount)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, errorResp{
 			Status:       "Failed",
@@ -93,7 +90,7 @@ func (h *handler) Transfer(c echo.Context) error {
 		})
 	}
 
-	err = updatePocket(begin, destPocket, destPocket.Balance+tDto.Amount)
+	err = tfService.updatePocket(destPocket, destPocket.Balance+tDto.Amount)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, errorResp{
 			Status:       "Failed",
@@ -101,7 +98,7 @@ func (h *handler) Transfer(c echo.Context) error {
 		})
 	}
 
-	tId, err := insertTransaction(h.db, tDto, "Success")
+	tId, err := tfService.insertTransaction(tDto, "Success")
 	if err != nil {
 		log.Println(err)
 		return c.JSON(http.StatusInternalServerError, errorResp{
@@ -118,44 +115,4 @@ func (h *handler) Transfer(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, resp)
-}
-
-func findPocket(db *sql.DB, pid string, pk *Pocket) error {
-	row := db.QueryRow(
-		"SELECT * FROM pockets where id=$1",
-		pid,
-	)
-	err := row.Scan(
-		&pk.ID,
-		&pk.Name,
-		&pk.Category,
-		&pk.Currency,
-		&pk.Balance,
-	)
-	return err
-}
-
-func insertTransaction(db *sql.DB, tDto *transferDto, status string) (int, error) {
-	row := db.QueryRow(
-		"INSERT INTO transactions (source_pid, dest_pid, amount, description, date, status) values ($1, $2, $3, $4, current_timestamp, $5)  RETURNING id",
-		tDto.SourcePocketId,
-		tDto.DestPocketId,
-		tDto.Amount,
-		tDto.Description,
-		status,
-	)
-
-	var resultId int
-	err := row.Scan(&resultId)
-	return resultId, err
-}
-
-func updatePocket(db *sql.Tx, p *Pocket, amount float64) error {
-	row := db.QueryRow(
-		"UPDATE pockets SET balance=$2 WHERE id=$1 RETURNING balance",
-		p.ID,
-		amount,
-	)
-	err := row.Scan(&p.Balance)
-	return err
 }

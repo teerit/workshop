a8d63045a95ed1f70c55109d40464270c74e3bb9
sirawatB <sirawat.is@nocnoc.com>
2023-01-15 14:53:55 +0700

    fix calculate name and add calculate test

--- pocket/calculate.go
+++ pocket/calculate.go
@@ -7,8 +7,8 @@ import (
 func Sub(x float64, y float64) float64 {
 	a := decimal.NewFromFloat(x)
 	b := decimal.NewFromFloat(y)
-	sum := a.Sub(b)
-	result, _ := sum.Float64()
+	sub := a.Sub(b)
+	result, _ := sub.Float64()
 	return result
 }
 
---
+++ pocket/calculate_test.go
@@ -0,0 +1,25 @@
+package pocket
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCalAdd(t *testing.T) {
+	a := 0.1
+	b := 0.2
+
+	result := Add(a, b)
+
+	assert.Equal(t, result, 0.3)
+
+}
+
+func TestCalSub(t *testing.T) {
+	a := 0.1
+	b := 0.2
+
+	result := Sub(b, a)
+
+	assert.Equal(t, result, 0.1)
+}
